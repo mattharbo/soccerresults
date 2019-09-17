91f11a170a888d982ec1e74d7c2699e7c087abc0
Matthieu Harbonnier <matthieu.harbonnier@gmail.com>
2019-09-18 01:10:24 +0200

    sqlite3 modification

---
+++ sqlite3 samples/data.sql
@@ -0,0 +1,12 @@
+INSERT INTO team (
+  'team_name',
+  'acronym',
+  'city')
+VALUES (
+  'Paris Saint-Germain',
+  'PSG',
+  'Paris'
+);
+
+insert into team ('team_name', 'acronym', 'city') values ('Lille OSC', 'LOSC', 'Lille');
+insert into team ('team_name', 'acronym', 'city') values ('Olympique Lyonnais', 'OL', 'Lyon');
---
+++ sqlite3 samples/schema.sql
@@ -0,0 +1,13 @@
+CREATE TABLE `team`
+(
+ `id`         INTEGER PRIMARY KEY AUTOINCREMENT,
+ `team_name`  NOT NULL ,
+ `acronym`    NOT NULL ,
+ `city`       NOT NULL
+);
+
+-- .mode column
+-- .headers on
+-- .schema
+-- .tables
+-- .read nomdufichier.sql
