6acb62b08df625e1774a5d2f753c59a575ce9dc0
Matthieu Harbonnier <matthieu.harbonnier@gmail.com>
2019-09-21 14:26:52 +0200

    3 pages & teams controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  root to:"pages#home", as: :home
+  get 'about', to:'pages#about', as: :about
+  get 'contact', to:'pages#contact', as: :contact
+  get 'teams', to:'teams#index', as: :teams
+  post 'teams', to: 'teams#create', as: :newteam
+  get 'teams/:id', to:'teams#show', as: :team
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
