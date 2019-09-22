fcf81a92ac061f5c502ab35e3b3867ad828923db
Matthieu Harbonnier <matthieu.harbonnier@gmail.com>
2019-09-22 19:56:57 +0200

    Add first game in fixtures table

--- app/models/fixture.rb
+++ app/models/fixture.rb
@@ -4,3 +4,10 @@ class Fixture < ApplicationRecord
   belongs_to :stadium
   belongs_to :season
 end
+
+
+# monacol = Fixture.create(home_team: Team.where(city:"Monaco")[0], away_team: Team.where(city:"Lyon")[0], stadium: Stadium.where(town:"Monaco")[0], season: Season.last)
+# monacol.status = "done"
+# monacol.date = "2019_08_09"
+# monacol.final_score = "0-3"
+# monacol.stage = 1
